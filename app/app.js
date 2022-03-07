f16484c347adec70ccb0b30a24f8f5764f36c7e6
juyeon Lee <ljy436400@naver.com>
2022-03-07 15:02:26 +0900

    13 완

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views", "./src/views");  //뷰 세팅, 뷰폴더명 적기
 app.set("view engine", "ejs"); //뷰 엔진 세팅
+app.use(express.static(`${__dirname}/src/public`));
 
 
 app.use("/", home);  // use==미들 웨어를 등록해주는 메서드 //라우터와 app.js 연결시키기코드
