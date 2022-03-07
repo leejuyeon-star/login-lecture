3c3ce88017d32946f5c6894066b69b80c313e12d
juyeon Lee <ljy436400@naver.com>
2022-03-07 16:07:57 +0900

    dom 을 이용한 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,19 @@
 "use strict"
 
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginbutton = document.querySelector("button");
+
+loginbutton.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+}
+
+
+console.log(id);
 console.log("hello");
